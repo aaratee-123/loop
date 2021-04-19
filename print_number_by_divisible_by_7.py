fefe3bf52a38198dfc19b17144fd766032e6e705
aaratee-123 <82765818+aaratee-123@users.noreply.github.com>
2021-04-19 19:42:57 +0530

    Add files via upload

---
+++ print_number_by_divisible_by_7.py
@@ -0,0 +1,6 @@
+#write a program to find number divisible by 7
+i=1
+while i<=100:
+    i=i+1
+    if i%7==0:
+        print(i)
---
+++ program_to_print1to5.py
@@ -0,0 +1,9 @@
+#write a program to print number 1 to 5
+# i=1
+# while i<6:
+#     print(i)
+#     i=i+1
+
+var=10.5
+var2=int(10.5)
+print(var2)
